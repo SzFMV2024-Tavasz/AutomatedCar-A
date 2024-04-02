835e70dec41d88180eab4948e39eaaadc972bc58
Nédli Péter <nedlipeter@stud.uni-obuda.hu>
2024-04-02 18:38:03 +0200

    Minor changes made due to too fast slowing down or speeding up and emergency break.

--- src/AutomatedCar/SystemComponents/NPCSpeedComponent.cs
+++ src/AutomatedCar/SystemComponents/NPCSpeedComponent.cs
@@ -11,33 +11,49 @@
     {
         NPCCar car;
         private int CarCurrentSpeed = 0;
+        private double helper = 0;
         
         public NPCSpeedComponent(VirtualFunctionBus virtualFunctionBus,NPCCar car) : base(virtualFunctionBus)
         {
             this.car = car;
         }
 
-        public const int EmergencyBrake = 3; //3 a vészfék
+        public const int EmergencyBrake = 2; //2 a vészfék
         public override void Process()
         {
             double CarFinalSpeed = car.Route.RoutePoints[car.Route.CurrentPointID].Speed; 
                 //car.Route.RoutePoints[car.Route.CurrentPointID].Speed;
             if (CarCurrentSpeed < CarFinalSpeed )
             {
-                CarCurrentSpeed = CarCurrentSpeed + 1;
-                car.Speed = CarCurrentSpeed;
+                helper = helper + 0.1;
+                if ( helper == 1 ) 
+                {
+                    CarCurrentSpeed = CarCurrentSpeed + 1;
+                    car.Speed = CarCurrentSpeed;
+                    helper = 0;
+                }
             }
             else if (CarCurrentSpeed > CarFinalSpeed)
             {
-                CarCurrentSpeed = CarCurrentSpeed - 1;
-                car.Speed = CarCurrentSpeed;
+                helper = helper + 0.1;
+                if (helper == 1)
+                {
+                    CarCurrentSpeed = CarCurrentSpeed - 1;
+                    car.Speed = CarCurrentSpeed;
+                    helper = 0;
+                }
             }
             else if (CarFinalSpeed == 0 && car.Route.RepeatAfterFinish == false)
             {
                 if (CarCurrentSpeed >= EmergencyBrake)
                 {
-                    CarCurrentSpeed -= EmergencyBrake;
-                    car.Speed = CarCurrentSpeed;
+                    helper += 0.1;
+                    if (helper == 2)
+                    {
+                        CarCurrentSpeed -= EmergencyBrake;
+                        car.Speed = CarCurrentSpeed;
+                        helper = 0;
+                    }
                 }
                 else 
                 {
