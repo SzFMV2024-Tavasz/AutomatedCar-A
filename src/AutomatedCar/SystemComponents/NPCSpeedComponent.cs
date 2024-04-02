421b035e618376842d5b422d512c03b3a928bf2b
Nédli Péter <nedlipeter@stud.uni-obuda.hu>
2024-04-02 22:45:14 +0200

    Changes made to process()

--- src/AutomatedCar/SystemComponents/NPCSpeedComponent.cs
+++ src/AutomatedCar/SystemComponents/NPCSpeedComponent.cs
@@ -18,49 +18,119 @@
             this.car = car;
         }
 
-        public const int EmergencyBrake = 2; //2 a vészfék
+        public const int EmergencyBrake = 8; //2 a vészfék
         public override void Process()
         {
-            double CarFinalSpeed = car.Route.RoutePoints[car.Route.CurrentPointID].Speed; 
                 //car.Route.RoutePoints[car.Route.CurrentPointID].Speed;
-            if (CarCurrentSpeed < CarFinalSpeed )
+            //if (CarCurrentSpeed < CarNextSpeed )
+            //{
+            //    helper = helper + 0.1;
+            //    if ( helper == 1 ) 
+            //    {
+            //        CarCurrentSpeed = CarCurrentSpeed + 1;
+            //        car.Speed = CarCurrentSpeed;
+            //        helper = 0;
+            //    }
+            //}
+            //else if (CarCurrentSpeed > CarNextSpeed)
+            //{
+            //    helper = helper + 0.1;
+            //    if (helper == 1)
+            //    {
+            //        CarCurrentSpeed = CarCurrentSpeed - 1;
+            //        car.Speed = CarCurrentSpeed;
+            //        helper = 0;
+            //    }
+            //}
+            //else if (CarNextSpeed == 0 && car.Route.RepeatAfterFinish == false)
+            //{
+            //    if (CarCurrentSpeed >= EmergencyBrake)
+            //    {
+            //        helper += 0.1;
+            //        if (helper == 2)
+            //        {
+            //            CarCurrentSpeed -= EmergencyBrake;
+            //            car.Speed = CarCurrentSpeed;
+            //            helper = 0;
+            //        }
+            //    }
+            //    else 
+            //    {
+            //        CarCurrentSpeed = 0;
+            //        car.Speed = CarCurrentSpeed;
+            //    }
+            //}
+            
+            var Next = car.Route.RoutePoints[car.Route.CurrentPointID + 1];
+            if (Next != car.Route.RoutePoints.Last() && car.Route.RepeatAfterFinish == true)
             {
-                helper = helper + 0.1;
-                if ( helper == 1 ) 
+                if (CarCurrentSpeed < Next.Speed)
                 {
-                    CarCurrentSpeed = CarCurrentSpeed + 1;
-                    car.Speed = CarCurrentSpeed;
-                    helper = 0;
-                }
-            }
-            else if (CarCurrentSpeed > CarFinalSpeed)
-            {
-                helper = helper + 0.1;
-                if (helper == 1)
-                {
-                    CarCurrentSpeed = CarCurrentSpeed - 1;
-                    car.Speed = CarCurrentSpeed;
-                    helper = 0;
-                }
-            }
-            else if (CarFinalSpeed == 0 && car.Route.RepeatAfterFinish == false)
-            {
-                if (CarCurrentSpeed >= EmergencyBrake)
-                {
-                    helper += 0.1;
-                    if (helper == 2)
+                    helper = helper + 0.1;
+                    if (helper == 1.5)
                     {
-                        CarCurrentSpeed -= EmergencyBrake;
-                        car.Speed = CarCurrentSpeed;
+                        CarCurrentSpeed = CarCurrentSpeed + 1;
+                        car.Speed = ConvertToPxS(CarCurrentSpeed);
                         helper = 0;
                     }
                 }
-                else 
+                else if (CarCurrentSpeed > Next.Speed)
+                {
+                    helper = helper + 0.1;
+                    if (helper == 1.5)
+                    {
+                        CarCurrentSpeed = CarCurrentSpeed - 1;
+                        car.Speed = ConvertToPxS(CarCurrentSpeed);
+                        helper = 0;
+                    }
+                }
+            }
+            else if(Next == car.Route.RoutePoints.Last() && car.Route.RepeatAfterFinish == true)
+            {
+                Next = car.Route.RoutePoints[0];
+                if (CarCurrentSpeed < Next.Speed)
+                {
+                    helper = helper + 0.1;
+                    if (helper == 1.5)
+                    {
+                        CarCurrentSpeed = CarCurrentSpeed + 1;
+                        car.Speed = ConvertToPxS(CarCurrentSpeed);
+                        helper = 0;
+                    }
+                }
+                else if (CarCurrentSpeed > Next.Speed)
+                {
+                    helper = helper + 0.1;
+                    if (helper == 1.5)
+                    {
+                        CarCurrentSpeed = CarCurrentSpeed - 1;
+                        car.Speed = ConvertToPxS(CarCurrentSpeed);
+                        helper = 0;
+                    }
+                }
+            }
+            else if (Next == car.Route.RoutePoints.Last() && car.Route.RepeatAfterFinish == false)
+            {
+                if (CarCurrentSpeed >= 8)
+                {
+                    car.Speed -= EmergencyBrake;
+                    helper = 0;
+                    
+                }
+                else
                 {
                     CarCurrentSpeed = 0;
                     car.Speed = CarCurrentSpeed;
                 }
             }
+
         }
+
+        private int ConvertToPxS(double value)
+        {
+            var changer = (int)Math.Round((value / 3.6) * 50,0);
+            return changer;
+        }
+
     }
 }
