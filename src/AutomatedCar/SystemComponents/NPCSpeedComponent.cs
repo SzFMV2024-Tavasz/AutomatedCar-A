4ed757c68e6361f1af87ebfe01fe08c8d2e13a96
Nédli Péter <nedlipeter@stud.uni-obuda.hu>
2024-04-02 17:14:20 +0200

    Task understood Process() written

--- src/AutomatedCar/SystemComponents/NPCSpeedComponent.cs
+++ src/AutomatedCar/SystemComponents/NPCSpeedComponent.cs
@@ -10,16 +10,38 @@
     internal class NPCSpeedComponent : SystemComponent
     {
         NPCCar car;
+        private int CarCurrentSpeed = 0;
+        
         public NPCSpeedComponent(VirtualFunctionBus virtualFunctionBus,NPCCar car) : base(virtualFunctionBus)
         {
             this.car = car;
         }
 
+        public const int EmergencyBrake = 3; //3 a vészfék
         public override void Process()
         {
-            double KiloMeterPerSec = (car.Speed * 25) / 3.6;
-            car.Speed = (int)(Math.Round(KiloMeterPerSec,0));
-
+            double CarFinalSpeed = car.Route.RoutePoints[car.Route.CurrentPointID].Speed; 
+                //car.Route.RoutePoints[car.Route.CurrentPointID].Speed;
+            if (CarCurrentSpeed < CarFinalSpeed )
+            {
+                CarCurrentSpeed = CarCurrentSpeed + 1;
+                car.Speed = CarCurrentSpeed;
+            }
+            else if (CarCurrentSpeed > CarFinalSpeed)
+            {
+                CarCurrentSpeed = CarCurrentSpeed - 1;
+            }
+            else if (CarFinalSpeed == 0 && car.Route.RepeatAfterFinish == false)
+            {
+                if (CarCurrentSpeed >= EmergencyBrake)
+                {
+                    CarCurrentSpeed -= EmergencyBrake;
+                }
+                else 
+                {
+                    CarCurrentSpeed = 0; 
+                }
+            }
         }
     }
 }
