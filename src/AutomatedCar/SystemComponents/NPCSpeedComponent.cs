2e3c0572a196126b2856620413a354ac2b8fb8f4
Nédli Péter <nedlipeter@stud.uni-obuda.hu>
2024-04-02 14:40:24 +0200

    NPCSpeedComponent added

---
+++ src/AutomatedCar/SystemComponents/NPCSpeedComponent.cs
@@ -0,0 +1,23 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using AutomatedCar.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class NPCSpeedComponent : SystemComponent
+    {
+        NPCCar car;
+        public NPCSpeedComponent(VirtualFunctionBus virtualFunctionBus,NPCCar car) : base(virtualFunctionBus)
+        {
+            this.car = car;
+        }
+
+        public override void Process()
+        {
+            
+        }
+    }
+}
