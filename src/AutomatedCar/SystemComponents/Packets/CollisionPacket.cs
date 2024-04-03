42a66beed7d51dfdcc39a20fc4bc27eddaeab3a0
asimoq <112816411+asimoq@users.noreply.github.com>
2024-04-03 19:22:22 +0200

    Implemented the packet for collision detection with an interface

---
+++ src/AutomatedCar/SystemComponents/Packets/CollisionPacket.cs
@@ -0,0 +1,20 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using ReactiveUI;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CollisionPacket : ReactiveObject, IReadOnlyCollisionPacket
+    {
+        private bool isCollided;
+
+        public bool IsCollided
+        {
+            get => this.isCollided;
+            set => this.RaiseAndSetIfChanged(ref this.isCollided, value);
+        }
+    }
+}
