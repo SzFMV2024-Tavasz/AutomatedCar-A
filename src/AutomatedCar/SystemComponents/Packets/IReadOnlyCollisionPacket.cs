5e9c095a26ebec4bb19e8372f7260ba9e63624d7
asimoq <112816411+asimoq@users.noreply.github.com>
2024-04-03 19:22:22 +0200

    Implemented the packet for collision detection with an interface

---
+++ src/AutomatedCar/SystemComponents/Packets/IReadOnlyCollisionPacket.cs
@@ -0,0 +1,13 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IReadOnlyCollisionPacket
+    {
+        bool IsCollided { get; }
+    }
+}
