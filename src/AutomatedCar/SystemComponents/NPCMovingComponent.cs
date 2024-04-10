fcd9c78a33d23ce29a6d90e6a003967d3406283c
Endl Levente <levente.endl@stud.uni-obuda.hu>
2024-04-10 21:27:30 +0200

    The NPC's route following got easier, as coordinates got changed to double.

--- src/AutomatedCar/SystemComponents/NPCMovingComponent.cs
+++ src/AutomatedCar/SystemComponents/NPCMovingComponent.cs
@@ -20,11 +20,7 @@
             next = car.Route.RoutePoints[car.Route.StartPointID + 1];
         }
 
-        double leftX;
-        double leftY;
         RoutePoint next;
-        int xDiff;
-        int yDiff;
 
         int sqrt2;
 
@@ -46,51 +42,33 @@
             {
                 sqrt2++;
             }
-            
 
+            double plusX = (double)car.Speed / GameBase.TicksPerSecond;
 
             if (car.X != next.X)
             {
                 if (car.X < next.X)
                 {
-                    double plusX = (double)car.Speed / GameBase.TicksPerSecond;
-                    leftX += plusX - Math.Floor(plusX);
-                    if (leftX > 1)
+
+                    if (car.X + plusX <= next.X)
                     {
-                        if (car.X + (int)Math.Floor(leftX) + (int)Math.Floor(plusX) <= next.X)
-                        {
-                            car.X += (int)Math.Floor(leftX) + (int)Math.Floor(plusX);
-                        }
-                        else
-                        {
-                            car.X = next.X;
-                        }
-                        leftX -= Math.Floor(leftX);
+                        car.X += plusX;
                     }
                     else
                     {
-                        car.X += (int)Math.Floor(plusX);
+                        car.X = next.X;
                     }
                 }
                 else if (car.X > next.X)
                 {
-                    double plusX = (double)car.Speed / GameBase.TicksPerSecond;
-                    leftX += plusX - Math.Floor(plusX);
-                    if (leftX > 1)
+
+                    if (car.X - plusX >= next.X)
                     {
-                        if (car.X - (int)Math.Floor(leftX) - (int)Math.Floor(plusX) >= next.X)
-                        {
-                            car.X -= (int)Math.Floor(leftX) + (int)Math.Floor(plusX);
-                        }
-                        else
-                        {
-                            car.X = next.X;
-                        }
-                        leftX -= Math.Floor(leftX);
+                        car.X -= plusX;
                     }
                     else
                     {
-                        car.X -= (int)Math.Floor(plusX);
+                        car.X = next.X;
                     }
                 }
                                 
@@ -99,53 +77,33 @@
             {
                 if (car.Y < next.Y)
                 {
-                    double plusY = (double)car.Speed / GameBase.TicksPerSecond;
-                    leftY += plusY - Math.Floor(plusY);
-                    if (leftY > 1)
+
+                    if (car.Y + plusX <= next.Y)
                     {
-                        if (car.Y + (int)Math.Floor(leftY) + (int)Math.Floor(plusY) <= next.Y)
-                        {
-                            car.Y += (int)Math.Floor(leftY) + (int)Math.Floor(plusY);
-                        }
-                        else
-                        {
-                            car.Y = next.Y;
-                        }
-                        leftY -= Math.Floor(leftY);
+                        car.Y += plusX;
                     }
                     else
                     {
-                        car.Y += (int)Math.Floor(plusY);
+                        car.Y = next.Y;
                     }
                 }
                 else if (car.Y > next.Y)
                 {
-                    double plusY = (double)car.Speed / GameBase.TicksPerSecond;
-                    leftY += plusY - Math.Floor(plusY);
-                    if (leftY > 1)
+
+                    if (car.Y - plusX >= next.Y)
                     {
-                        if (car.Y - (int)Math.Floor(leftY) - (int)Math.Floor(plusY) >= next.Y)
-                        {
-                            car.Y -= (int)Math.Floor(leftY) + (int)Math.Floor(plusY);
-                        }
-                        else
-                        {
-                            car.Y = next.Y;
-                        }
-                        leftY -= Math.Floor(leftY);
+                        car.Y -= plusX;
                     }
                     else
                     {
-                        car.Y -= (int)Math.Floor(plusY);
+                        car.Y = next.Y;
                     }
                 }
-              
 
             }
             if (car.X == next.X && car.Y == next.Y)
             {
-                leftX = 0;
-                leftY = 0;
+
                 if (car.Route.RoutePoints[car.Route.CurrentPointID + 1] != car.Route.RoutePoints.Last())
                 {
                     car.Route.CurrentPointID = car.Route.RoutePoints.IndexOf(car.Route.RoutePoints.Single(t => t == next));
@@ -159,8 +117,7 @@
                 {
                     next = car.Route.RoutePoints[car.Route.CurrentPointID + 1];
                 }
-                xDiff = (int)next.X - (int)car.X;
-                yDiff = (int)next.Y - (int)car.Y;
+
             }
 
 
