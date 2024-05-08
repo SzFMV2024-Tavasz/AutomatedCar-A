28404ee1fb22c2e04772b48eef371383afe0660b
Nédli Péter <nedlipeter@stud.uni-obuda.hu>
2024-05-08 19:03:08 +0200

    changed CarInFront

--- src/AutomatedCar/SystemComponents/AdaptiveTempomatComponent.cs
+++ src/AutomatedCar/SystemComponents/AdaptiveTempomatComponent.cs
@@ -2,6 +2,7 @@
 {
     using AutomatedCar.Models;
     using AutomatedCar.SystemComponents.Packets;
+    using AutomatedCar.SystemComponents.Packets.Radar;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -14,13 +15,14 @@
 
 
         AdaptiveTempomatPacket ATPacket;
-
-
+        RadarPacket RPacket;
+        
         public AdaptiveTempomatComponent(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
         {
             
             ATPacket = new AdaptiveTempomatPacket();
             virtualFunctionBus.TempomatPacket = ATPacket;
+            virtualFunctionBus.RadarPacket = RPacket;
             ATPacket.CarInFront = null;
             ATPacket.IsItOn = false;
             
@@ -29,30 +31,43 @@
 
         public override void Process()
         {
+            
             if (ATPacket.IsItOn)
             {
+                 ATPacket.CarInFront =  CarInFront();
                 if (ATPacket.CarInFront != null)
                 {
-                    CarInFront();
+
                 }
             }
         }
 
-
-        public void CarInFront()
+        public NPCCar CarInFront()
         {
-            //if (ATPacket.IsItOn)
-            //{
-            //    if (ATPacket.CarInFront != null)
-            //    {
-                    if (ATPacket.CarInFront.Speed <= ATPacket.WantedSpeed)
+            var relevantObjects = RPacket.RelevantObjects;
+            if (relevantObjects != null) 
+            {
+                foreach (var rObj in relevantObjects)
+                {
+                    if (rObj.GetType() is NPCCar)
                     {
-                        ATPacket.WantedSpeed = (int)ATPacket.CarInFront.Speed;
+                        if (Math.Abs((((rObj as NPCCar).Rotation)- World.Instance.ControlledCar.Rotation)) <= 20)
+                        {
+                            return (rObj as NPCCar);
+                        }
+                        
                     }
                     
-            //    }
-            //}
+                }
+            }
+
         }
+
+        public int DistanceBetweenCars()
+        {
+
+        }
+
 
     }
 }
